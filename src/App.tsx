342813698570eb2e64a1630c20e970a43913e647
Jakub Kozłowski <kubukoz@gmail.com>
2019-10-08 16:49:57 +0200

    Hello world 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-type WithName = { name: String }
+type WithName = { name: String, bame: String }
 
 const App: React.FC<WithName> = (obj) => {
   console.log(obj.name)
@@ -12,7 +12,7 @@ const App: React.FC<WithName> = (obj) => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. {obj.name}
+          Edit <code>src/App.tsx</code> and save to reload. {obj.name} {obj.bame}
         </p>
         <a
           className="App-link"
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App name="foo" />, document.getElementById('root'));
+const propz = { name: "foo", bame: "bar" }
+ReactDOM.render(<App {...propz} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
