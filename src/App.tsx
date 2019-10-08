a9b7150cdcd6f79dae513df30df2ee2a5fe5b0b0
Jakub Kozłowski <kubukoz@gmail.com>
2019-10-08 16:46:21 +0200

    Hello world in react

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+type WithName = { name: String }
+
+const App: React.FC<WithName> = (obj) => {
+  console.log(obj.name)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. {obj.name}
         </p>
         <a
           className="App-link"
